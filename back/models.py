6d6c221e4522289d362e7067b38f13e9832c44b4
jerosantamariai <jerosantamariai@gmail.com>
2021-04-04 13:17:47 -0400

    ready to hack

---
+++ back/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Roles (db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    rolename = db.Column(db.String(50), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rolename": self.rolename
+        }
+
+class Users (db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    lastname = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=False)
+    avatar = db.Column(db.String(100), nullable=True, default='defaultavatar.jpg')
+    phone = db.Column(db.String(12), nullable=True)
+    createdate = db.Column(db.DateTime, default=datetime.now())
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship(Roles)
+    # appointment = db.relationship("Appointment", backref="appointment", cascade="delete")
+
+    def serialize(self):
+        # appointment = []
+        # appointment = list(map(lambda appoint: appoint.serialize(), self.appointment))
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "avatar": self.avatar,
+            "phone": self.phone,
+            "createdate": self.createdate,
+            "role": self.role.serialize(),
+            # "appointment": appointment,
+        }
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import injectContext from './store/appContext';
+import Home from './views/home';
+import NotFound from './views/notfound';
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <div className="row">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default injectContext(App)
